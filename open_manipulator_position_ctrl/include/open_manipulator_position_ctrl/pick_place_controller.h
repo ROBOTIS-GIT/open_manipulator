0f1b76e45c72c81b1db4703a75cc1ac492fe9972
Darby Lim <thlim@robotis.com>
2018-03-27 19:37:46 +0900

    add connection gazebo with moveit

--- open_manipulator_position_ctrl/include/open_manipulator_position_ctrl/pick_place_controller.h
+++ open_manipulator_position_ctrl/include/open_manipulator_position_ctrl/pick_place_controller.h
@@ -14,29 +14,30 @@
 * limitations under the License.
 *******************************************************************************/
 
-/* Authors: Taehoon Lim (Darby) */
+/* Authors: Taehun Lim (Darby) */
 
 #ifndef OPEN_MANIPULATOR_PICK_AND_PLACE_CONTROLLER_H
 #define OPEN_MANIPULATOR_PICK_AND_PLACE_CONTROLLER_H
 
+#include <ros/ros.h>
+
 #include <moveit/move_group_interface/move_group_interface.h>
-#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit/planning_interface/planning_interface.h>
 
-#include <moveit_msgs/DisplayRobotState.h>
 #include <moveit_msgs/DisplayTrajectory.h>
-
-#include <moveit_msgs/AttachedCollisionObject.h>
-#include <moveit_msgs/CollisionObject.h>
-
-#include <moveit_visual_tools/moveit_visual_tools.h>
 
 #include <std_msgs/Float64.h>
 #include <std_msgs/String.h>
 
-#include <geometry_msgs/PoseStamped.h>
 #include <sensor_msgs/JointState.h>
 
+#include "open_manipulator_msgs/JointPose.h"
+#include "open_manipulator_msgs/KinematicsPose.h"
+
 #include <boost/thread.hpp>
+
+#include <eigen3/Eigen/Eigen>
 
 namespace open_manipulator
 {
@@ -47,6 +48,20 @@ namespace open_manipulator
 #define RIGHT_PALM  1
 
 #define ITERATION_FREQUENCY 25 //Hz
+
+typedef struct
+{
+  std::string name;
+  uint8_t id;
+  uint8_t joint_num;
+} Joint;
+
+typedef struct
+{
+  uint16_t waypoints;                                  // planned number of via-points
+  ros::Duration time_from_start;                       // planned movement time
+  Eigen::MatrixXd planned_path_positions;              // planned position trajectory
+} PlannedPathInfo;
 
 class PickAndPlaceController
 {
@@ -64,16 +79,35 @@ class PickAndPlaceController
 
   // ROS Subscribers
   ros::Subscriber gazebo_present_joint_position_sub_;
+  ros::Subscriber display_planned_path_sub_;
+  ros::Subscriber target_joint_pose_sub_;
+  ros::Subscriber target_kinematics_pose_sub_;
+
   // ROS Service Server
 
   // ROS Service Client
 
   // Joint states
-  std::map<std::string, uint8_t> joint_id_;
-  Eigen::VectorXd present_joint_position_;
+  Joint joint_[4];
+  Joint gripper_;
+
+//  Eigen::VectorXd present_joint_position_;
+//  Eigen::VectorXd goal_joint_position_;
+  Eigen::MatrixXd goal_trajectory_;
+
+  // MoveIt! interface
+  std::string planning_group_;
+  moveit::planning_interface::MoveGroupInterface *move_group;
+  moveit_msgs::DisplayTrajectory trajectory_msg_;
+  PlannedPathInfo planned_path_info_;
 
   // thread
   boost::thread* trajectory_generate_thread_;
+
+  // Process state variables
+  bool is_moving_;
+  double   move_time_;
+  uint16_t all_time_steps_;
 
  public:
   PickAndPlaceController();
@@ -81,14 +115,17 @@ class PickAndPlaceController
 
   void process(void);
 
-  // Process state variables
-  bool is_moving_;
-
  private:
   void initPublisher(bool using_gazebo);
   void initSubscriber(bool using_gazebo);
 
+  void moveItTrajectoryGenerateThread();
+
   void gazeboPresentJointPositionMsgCallback(const sensor_msgs::JointState::ConstPtr &msg);
+  void displayPlannedPathMsgCallback(const moveit_msgs::DisplayTrajectory::ConstPtr &msg);
+
+  void targetJointPoseMsgCallback(const open_manipulator_msgs::JointPose::ConstPtr &msg);
+  void targetKinematicsPoseMsgCallback(const open_manipulator_msgs::KinematicsPose::ConstPtr &msg);
 };
 }
 
--- open_manipulator_position_ctrl/src/pick_place_controller.cpp
+++ open_manipulator_position_ctrl/src/pick_place_controller.cpp
@@ -29,12 +29,23 @@ PickAndPlaceController::PickAndPlaceController()
   nh_.getParam("gazebo", using_gazebo_);
   nh_.getParam("robot_name", robot_name_);
 
-  joint_id_["joint1"] = 0;
-  joint_id_["joint2"] = 1;
-  joint_id_["joint3"] = 2;
-  joint_id_["joint4"] = 3;
+  joint_[0].name = "joint1";
+  joint_[1].name = "joint2";
+  joint_[2].name = "joint3";
+  joint_[3].name = "joint4";
 
-  present_joint_position_ = Eigen::VectorXd::Zero(JOINT_NUM+GRIP_NUM);
+  gripper_.name = "gripper";
+
+//  present_joint_position_ = Eigen::VectorXd::Zero(JOINT_NUM+GRIP_NUM);
+//  goal_joint_position_    = Eigen::VectorXd::Zero(JOINT_NUM);
+
+  planned_path_info_.waypoints = 10;
+  planned_path_info_.time_from_start = ros::Duration(10.0);
+  planned_path_info_.planned_path_positions = Eigen::MatrixXd::Zero(planned_path_info_.waypoints, JOINT_NUM);
+
+  planning_group_ = "arm";
+
+  move_group = new moveit::planning_interface::MoveGroupInterface(planning_group_);
 
   initPublisher(using_gazebo_);
   initSubscriber(using_gazebo_);
@@ -51,11 +62,11 @@ void PickAndPlaceController::initPublisher(bool using_gazebo)
   if (using_gazebo)
   {
     ROS_INFO("SET Gazebo Simulation Mode");
-    for (auto it = joint_id_.begin(); it != joint_id_.end(); it++)
+
+    for (uint8_t index = 0; index < JOINT_NUM; index)
     {
-      std::string joint_name = it->first;
-      gazebo_goal_joint_position_pub_[joint_id_[joint_name]]
-        = nh_.advertise<std_msgs::Float64>("/" + robot_name_ + "/" + joint_name + "_position/command", 10);
+      gazebo_goal_joint_position_pub_[index]
+        = nh_.advertise<std_msgs::Float64>("/" + robot_name_ + "/" + joint_[index].name + "_position/command", 10);
     }
 
     gazebo_gripper_position_pub_[0]  = nh_.advertise<std_msgs::Float64>("/" + robot_name_ + "/grip_joint_position/command", 10);
@@ -70,94 +81,174 @@ void PickAndPlaceController::initSubscriber(bool using_gazebo)
     gazebo_present_joint_position_sub_ = nh_.subscribe("/" + robot_name_ + "/joint_states", 10,
                                                        &PickAndPlaceController::gazeboPresentJointPositionMsgCallback, this);
   }
+
+  target_joint_pose_sub_ = nh_.subscribe("/" + robot_name_ + "/target_pose", 10,
+                                         &PickAndPlaceController::targetJointPoseMsgCallback, this);
+
+  target_kinematics_pose_sub_ = nh_.subscribe("/" + robot_name_ + "/kinematics_pose", 10,
+                                         &PickAndPlaceController::targetKinematicsPoseMsgCallback, this);
+
+
+  display_planned_path_sub_ = nh_.subscribe("/move_group/display_planned_path", 10,
+                                            &PickAndPlaceController::displayPlannedPathMsgCallback, this);
 }
 
 void PickAndPlaceController::gazeboPresentJointPositionMsgCallback(const sensor_msgs::JointState::ConstPtr &msg)
 {
-  for (auto index = 0; index < JOINT_NUM + GRIP_NUM; index++)
-    present_joint_position_(index) = msg->position.at(index);
+//  for (auto index = 0; index < JOINT_NUM + GRIP_NUM; index++)
+//    present_joint_position_(index) = msg->position.at(index);
+}
+
+void PickAndPlaceController::targetJointPoseMsgCallback(const open_manipulator_msgs::JointPose::ConstPtr &msg)
+{
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  const robot_state::JointModelGroup *joint_model_group = move_group->getCurrentState()->getJointModelGroup(planning_group_);
+
+  moveit::core::RobotStatePtr current_state = move_group->getCurrentState();
+
+  std::vector<double> joint_group_positions;
+  current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
+
+//  for (uint8_t index = 0; index < JOINT_NUM; index++)
+//  {
+//    if (msg->joint_name[index] == ("joint" + std::to_string((index+1))))
+//    {
+//      joint_group_positions[index] = msg->position[index];
+
+//      ROS_WARN("%lf", joint_group_positions[index]);
+//    }
+//  }
+
+  joint_group_positions[0] =  0.0;                  // radians
+  joint_group_positions[1] = -70.0 * (M_PI/180.0);  // radians
+  joint_group_positions[2] =  30.0 * (M_PI/180.0);  // radians
+  joint_group_positions[3] =  40.0 * (M_PI/180.0);  // radians
+
+  move_group->setJointValueTarget(joint_group_positions);
+
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+  bool success = (move_group->plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+  ROS_INFO("Planning (joint space goal) %s", success ? "SUCCESS" : "FAILED");
+
+  spinner.stop();
+}
+
+void PickAndPlaceController::targetKinematicsPoseMsgCallback(const open_manipulator_msgs::KinematicsPose::ConstPtr &msg)
+{
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  geometry_msgs::Pose target_pose;
+  target_pose.orientation.w =  0.0;
+  target_pose.position.x    =  0.28;
+  target_pose.position.y    = -0.7;
+  target_pose.position.z    =  1.0;
+  move_group->setPoseTarget(target_pose);
+
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+  bool success = (move_group->plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+  ROS_INFO("Planning (Cartesian space goal) %s", success ? "SUCCESS" : "FAILED");
+
+  spinner.stop();
+}
+
+void PickAndPlaceController::displayPlannedPathMsgCallback(const moveit_msgs::DisplayTrajectory::ConstPtr &msg)
+{
+  trajectory_msg_ = *msg;
+
+  trajectory_generate_thread_ = new boost::thread(boost::bind(&PickAndPlaceController::moveItTrajectoryGenerateThread, this));
+  delete trajectory_generate_thread_;
+}
+
+void PickAndPlaceController::moveItTrajectoryGenerateThread()
+{
+  for (uint16_t tra_index = 0; tra_index < trajectory_msg_.trajectory.size(); tra_index++)
+  {
+    planned_path_info_.waypoints = trajectory_msg_.trajectory[tra_index].joint_trajectory.points.size();
+
+    planned_path_info_.planned_path_positions.resize(planned_path_info_.waypoints, JOINT_NUM);
+
+    for (uint16_t point_index = 0; point_index < planned_path_info_.waypoints; point_index++)
+    {
+      planned_path_info_.time_from_start = trajectory_msg_.trajectory[tra_index].joint_trajectory.points[point_index].time_from_start;
+
+      for (uint16_t joint_index = 0; joint_index < trajectory_msg_.trajectory[tra_index].joint_trajectory.joint_names.size(); joint_index++)
+      {
+        float joint_position = trajectory_msg_.trajectory[tra_index].joint_trajectory.points[point_index].positions[joint_index];
+
+        planned_path_info_.planned_path_positions.coeffRef(point_index , joint_index) = joint_position;
+      }
+    }
+  }
+
+  move_time_ = planned_path_info_.time_from_start.toSec();
+
+  all_time_steps_ = planned_path_info_.waypoints;
+
+  ros::WallDuration sleep_time(0.5);
+  sleep_time.sleep();
+
+  goal_trajectory_.resize(planned_path_info_.waypoints, JOINT_NUM);
+  goal_trajectory_ = planned_path_info_.planned_path_positions;
+  ROS_INFO("Get Joint Trajectory");
+
+  is_moving_  = true;
+
+  ROS_INFO("Send Motion Trajectory");
 }
 
 void PickAndPlaceController::process(void)
 {
+  static uint16_t step_cnt = 0;
+  sensor_msgs::JointState goal_joint_state;
+  std_msgs::Float64 goal_joint_position;
+
   if (is_moving_)
   {
-    for (int index = 0; index < JOINT_NUM; index++)
+    if (using_gazebo_)
     {
-      std_msgs::Float64 joint_position;
-      joint_position.data = present_joint_position_(index);
-      gazebo_goal_joint_position_pub_[index].publish(joint_position);
+      for (uint8_t index = 0; index < JOINT_NUM; index++)
+      {
+        goal_joint_state.name.push_back(joint_[index].name);
+        goal_joint_state.position.push_back(goal_trajectory_(step_cnt, index));
 
-      ROS_INFO("joint_position[%d] = %lf", index, joint_position.data);
+        goal_joint_position.data = goal_joint_state.position.at(index);
+        gazebo_goal_joint_position_pub_[index].publish(goal_joint_position);
+      }
     }
+
+    if (step_cnt >= all_time_steps_)
+    {
+      is_moving_ = false;
+      step_cnt  = 0;
+
+      ROS_INFO("End Trajectory");
+    }
+    else
+      step_cnt++;
   }
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "pick_and_place_controller_for_OpenManipulator");
+  PickAndPlaceController controller;
 
-//  PickAndPlaceController controller;
+  ros::Rate loop_rate(ITERATION_FREQUENCY);
 
-//  ros::Rate loop_rate(ITERATION_FREQUENCY);
+  while (ros::ok())
+  {
+    controller.process();
 
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
-
-  static const std::string PLANNING_GROUP = "arm";
-
-  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
-
-  const robot_state::JointModelGroup *joint_model_group =
-   move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
-
-  ROS_INFO_NAMED("tutorial", "Reference frame: %s", move_group.getPlanningFrame().c_str());
-  ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group.getEndEffectorLink().c_str());
-
-  moveit::core::RobotStatePtr current_state = move_group.getCurrentState();
-
-  std::vector<double> joint_group_positions;
-  current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
-
-  joint_group_positions[0] =  0.0;  // radians
-  joint_group_positions[1] = -70.0 * (M_PI/180.0);  // radians
-  joint_group_positions[2] =  30.0 * (M_PI/180.0);  // radians
-  joint_group_positions[3] =  40.0 * (M_PI/180.0);  // radians
-  move_group.setJointValueTarget(joint_group_positions);
-
-  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-
-  bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-  ROS_INFO_NAMED("tutorial", "Visualizing plan (joint space goal) %s", success ? "" : "FAILED");
-
-  move_group.move();
-
-//  controller.is_moving_ = true;
-
-//  while (ros::ok())
-//  {
-//    controller.process();
-
-//    ros::spinOnce();
-//    loop_rate.sleep();
-//  }
-
-//  return 0;
-
-//  geometry_msgs::Pose target_pose1;
-//  target_pose1.orientation.w = 0.0;
-//  target_pose1.position.x = 0.28;
-//  target_pose1.position.y = -0.7;
-//  target_pose1.position.z = 1.0;
-//  move_group.setPoseTarget(target_pose1);
-
-//  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-
-//  bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-
-//  ROS_INFO_NAMED("tutorial", "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
-
-  ros::shutdown();
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
 
   return 0;
 }
