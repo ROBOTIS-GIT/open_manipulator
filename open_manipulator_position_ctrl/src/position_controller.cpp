4bb0ef4dc2f9e0facdccee3acbc66ca7100f0c9f
Darby Lim <thlim@robotis.com>
2018-03-27 19:38:02 +0900

    delete annotation

--- open_manipulator_position_ctrl/src/position_controller.cpp
+++ open_manipulator_position_ctrl/src/position_controller.cpp
@@ -222,24 +222,6 @@ void PositionController::moveGroupActionFeedbackMsgCallback(const moveit_msgs::M
 
 void PositionController::displayPlannedPathMsgCallback(const moveit_msgs::DisplayTrajectory::ConstPtr &msg)
 {
-  /*
-  # The model id for which this path has been generated
-
-  string model_id
-
-  # The representation of the path contains position values for all the joints that are moving along the path;
-  # a sequence of trajectories may be specified
-
-  RobotTrajectory[] trajectory
-
-  # The robot state is used to obtain positions for all/some of the joints of the robot.
-  # It is used by the path display node to determine the positions of the joints that are not specified in the joint path message above.
-  # If the robot state message contains joint position information for joints that are also mentioned in the joint path message,
-  # the positions in the joint path message will overwrite the positions specified in the robot state message.
-
-  RobotState trajectory_start
-  */
-
   motionPlanningTool_->moveit_msg_ = *msg;
 
   trajectory_generate_thread_ = new boost::thread(boost::bind(&PositionController::moveItTragectoryGenerateThread, this));
