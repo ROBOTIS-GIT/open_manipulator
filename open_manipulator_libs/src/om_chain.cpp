7019663ad5f5740418b8c68ce721f30028ab53e8
Hye-Jong KIM <hjkim@robotis.com>
2018-11-02 20:33:05 +0900

    update new RML

--- open_manipulator_libs/src/om_chain.cpp
+++ open_manipulator_libs/src/om_chain.cpp
@@ -33,7 +33,7 @@ void OM_CHAIN::initManipulator()
                WORLD, // parent name
                COMP2, // child name
                RM_MATH::makeVector3(-0.278, 0.0, 0.017), // relative position
-               RM_MATH::convertRPYToQuaternion(0.0, 0.0, 0.0), // relative orientation
+               RM_MATH::convertRPYToRotation(0.0, 0.0, 0.0), // relative orientation
                Z_AXIS, // axis of rotation
                11); // actuator id
 
@@ -41,7 +41,7 @@ void OM_CHAIN::initManipulator()
                COMP1, // parent name
                COMP3, // child name
                RM_MATH::makeVector3(0.0, 0.0, 0.058), // relative position
-               RM_MATH::convertRPYToQuaternion(0.0, 0.0, 0.0), // relative orientation
+               RM_MATH::convertRPYToRotation(0.0, 0.0, 0.0), // relative orientation
                Y_AXIS, // axis of rotation
                12); // actuator id
 
@@ -49,7 +49,7 @@ void OM_CHAIN::initManipulator()
                COMP2, // parent name
                COMP4, // child name
                RM_MATH::makeVector3(0.024, 0.0, 0.128), // relative position
-               RM_MATH::convertRPYToQuaternion(0.0, 0.0, 0.0), // relative orientation
+               RM_MATH::convertRPYToRotation(0.0, 0.0, 0.0), // relative orientation
                Y_AXIS, // axis of rotation
                13); // actuator id
 
@@ -57,14 +57,14 @@ void OM_CHAIN::initManipulator()
                COMP3, // parent name
                TOOL, // child name
                RM_MATH::makeVector3(0.124, 0.0, 0.0), // relative position
-               RM_MATH::convertRPYToQuaternion(0.0, 0.0, 0.0), // relative orientation
+               RM_MATH::convertRPYToRotation(0.0, 0.0, 0.0), // relative orientation
                Y_AXIS, // axis of rotation
                14); // actuator id
 
   addTool(TOOL, // my name
           COMP4, // parent name
           RM_MATH::makeVector3(0.130, 0.0, 0.0), // relative position
-          RM_MATH::convertRPYToQuaternion(0.0, 0.0, 0.0), // relative orientation
+          RM_MATH::convertRPYToRotation(0.0, 0.0, 0.0), // relative orientation
           15, // actuator id
           1.0f); // Change unit from `meter` to `radian`
 
@@ -74,8 +74,6 @@ void OM_CHAIN::initManipulator()
 
   ////////// joint actuator init.
   actuator_ = new OM_DYNAMIXEL::JointDynamixel();
-  addJointActuator(JOINT_DYNAMIXEL, actuator_);
-
   // communication setting argument
   std::string dxl_comm_arg[2] = {"/dev/ttyUSB0", "1000000"};
   void *p_dxl_comm_arg = &dxl_comm_arg;
@@ -85,8 +83,7 @@ void OM_CHAIN::initManipulator()
   jointDxlId.push_back(12);
   jointDxlId.push_back(13);
   jointDxlId.push_back(14);
-
-  jointActuatorInit(JOINT_DYNAMIXEL, jointDxlId, p_dxl_comm_arg);
+  addJointActuator(JOINT_DYNAMIXEL, actuator_, jointDxlId, p_dxl_comm_arg);
 
   // set joint actuator control mode
   std::string joint_dxl_mode_arg = "position_mode";
@@ -95,11 +92,10 @@ void OM_CHAIN::initManipulator()
 
   ////////// tool actuator init.
   tool_ = new OM_DYNAMIXEL::GripperDynamixel();
-  addToolActuator(TOOL_DYNAMIXEL, tool_);
 
-  // set gripper actuator id
   uint8_t gripperDxlId = 15;
-  toolActuatorInit(TOOL_DYNAMIXEL, gripperDxlId, p_dxl_comm_arg);
+  addToolActuator(TOOL_DYNAMIXEL, tool_, gripperDxlId, p_dxl_comm_arg);
+
 
   // set gripper actuator control mode
   std::string gripper_dxl_mode_arg = "current_based_position_mode";
@@ -124,14 +120,14 @@ void OM_CHAIN::initManipulator()
   addDrawingTrajectory(DRAWING_HEART, &heart_);
 
   ////////// manipulator trajectory & control time initialization
-  getManipulator()->setAllJointActuatorValue(receiveAllJointActuatorValue(jointDxlId));
+  receiveAllJointActuatorValue();
   initTrajectoryWayPoint();
   setControlTime(ACTUATOR_CONTROL_TIME);
 }
 
 void OM_CHAIN::chainProcess(double present_time)
 {
-  getManipulator()->setAllJointActuatorValue(receiveAllJointActuatorValue(jointDxlId));
+  receiveAllJointActuatorValue();
   forward();
   trajectoryControllerLoop(present_time);
 }
