eeac1cc4947e449b71e98d0c8704d7a6ba225acf
Ryan Jaehyun Shim <38637417+rjshim@users.noreply.github.com>
2020-07-01 16:27:27 +0900

    Update open_manipulator_x_controller.cpp

--- open_manipulator_x_controller/src/open_manipulator_x_controller.cpp
+++ open_manipulator_x_controller/src/open_manipulator_x_controller.cpp
@@ -564,7 +564,7 @@ int main(int argc, char **argv)
 
   std::string usb_port = "/dev/ttyUSB0";
   std::string baud_rate = "1000000";
-  if (argc == 3)
+  if (argc >= 3)
   {
     usb_port = argv[1];
     baud_rate = argv[2];
