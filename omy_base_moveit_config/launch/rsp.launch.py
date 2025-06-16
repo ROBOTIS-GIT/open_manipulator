e8de8f2f5d3fa5f19c956b7865f33ddfc3a36604
Wonho Yun <ywh@robotis.com>
2025-06-16 20:30:06 +0900

    Modified the description meshes and launch
    Signed-off-by: Wonho Yun <ywh@robotis.com>

--- omy_base_moveit_config/launch/rsp.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_rsp_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("omy_base", package_name="omy_base_moveit_config").to_moveit_configs()
-    return generate_rsp_launch(moveit_config)
--- omy_base_moveit_config/launch/setup_assistant.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_setup_assistant_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("omy_base", package_name="omy_base_moveit_config").to_moveit_configs()
-    return generate_setup_assistant_launch(moveit_config)
