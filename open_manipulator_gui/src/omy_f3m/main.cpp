e50e9753cc82ee31eaac5e9f8b47fef012e1aa5b
Wonho Yun <ywh@robotis.com>
2025-06-19 10:33:51 +0900

    Modified GUI pkg
    Signed-off-by: Wonho Yun <ywh@robotis.com>

---
+++ open_manipulator_gui/src/omy_f3m/main.cpp
@@ -0,0 +1,31 @@
+// Copyright 2024 ROBOTIS CO., LTD.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: Ryan Shim, Sungho Woo, Wonho Yun, Woojin Wie
+
+#include <QtGui>
+#include <QApplication>
+#include "../include/omy_f3m_gui/main_window.hpp"
+#include "omy_f3m_gui/qnode.hpp"
+
+
+int main(int argc, char ** argv)
+{
+  QApplication app(argc, argv);
+  omy_f3m_gui::MainWindow w(argc, argv);
+  w.show();
+  app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
+  int result = app.exec();
+  return result;
+}
