d2160d75a9b0b86310068c73c5193fb09b3dc7c1
HEEWONLEE7 <7dlgmldnjs7@gmail.com>
2025-10-21 12:02:19 +0900

    chore: bump package version to 4.0.10 for open_manipulator_bringup and open_manipulator_teleop
    Signed-off-by: HEEWONLEE7 <7dlgmldnjs7@gmail.com>

--- open_manipulator_teleop/setup.py
+++ open_manipulator_teleop/setup.py
@@ -8,7 +8,7 @@ package_name = 'open_manipulator_teleop'
 
 setup(
     name=package_name,
-    version='4.0.9',
+    version='4.0.10',
     packages=find_packages(exclude=[]),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
