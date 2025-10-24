3a0c2baa51979520850f2bc9063df69062d69dd2
HEEWONLEE7 <7dlgmldnjs7@gmail.com>
2025-10-24 14:40:26 +0900

    chore: update package versions to 4.1.0 across all modules
    Signed-off-by: HEEWONLEE7 <7dlgmldnjs7@gmail.com>

--- open_manipulator_teleop/setup.py
+++ open_manipulator_teleop/setup.py
@@ -8,7 +8,7 @@ package_name = 'open_manipulator_teleop'
 
 setup(
     name=package_name,
-    version='4.0.10',
+    version='4.1.0',
     packages=find_packages(exclude=[]),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
