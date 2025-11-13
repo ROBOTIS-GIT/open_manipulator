ab4ce130374bb18590e81129798818158ce9a4b7
Woojin Wie <wwj@robotis.com>
2025-11-13 15:28:31 +0900

    refactor: rename fake hardware and sensor command arguments to mock in launch and URDF files

--- open_manipulator_bringup/launch/omx_f.launch.py
+++ open_manipulator_bringup/launch/omx_f.launch.py
@@ -47,14 +47,14 @@ def generate_launch_description():
             description='Start robot in Gazebo simulation.',
         ),
         DeclareLaunchArgument(
-            'use_fake_hardware',
+            'use_mock_hardware',
             default_value='false',
-            description='Use fake hardware mirroring command.',
+            description='Use mock hardware mirroring command.',
         ),
         DeclareLaunchArgument(
-            'fake_sensor_commands',
+            'mock_sensor_commands',
             default_value='false',
-            description='Enable fake sensor commands.',
+            description='Enable mock sensor commands.',
         ),
         DeclareLaunchArgument(
             'port_name',
@@ -82,8 +82,8 @@ def generate_launch_description():
     start_rviz = LaunchConfiguration('start_rviz')
     prefix = LaunchConfiguration('prefix')
     use_sim = LaunchConfiguration('use_sim')
-    use_fake_hardware = LaunchConfiguration('use_fake_hardware')
-    fake_sensor_commands = LaunchConfiguration('fake_sensor_commands')
+    use_mock_hardware = LaunchConfiguration('use_mock_hardware')
+    mock_sensor_commands = LaunchConfiguration('mock_sensor_commands')
     port_name = LaunchConfiguration('port_name')
     init_position = LaunchConfiguration('init_position')
     ros2_control_type = LaunchConfiguration('ros2_control_type')
@@ -106,11 +106,11 @@ def generate_launch_description():
         'use_sim:=',
         use_sim,
         ' ',
-        'use_fake_hardware:=',
-        use_fake_hardware,
+        'use_mock_hardware:=',
+        use_mock_hardware,
         ' ',
-        'fake_sensor_commands:=',
-        fake_sensor_commands,
+        'mock_sensor_commands:=',
+        mock_sensor_commands,
         ' ',
         'port_name:=',
         port_name,
